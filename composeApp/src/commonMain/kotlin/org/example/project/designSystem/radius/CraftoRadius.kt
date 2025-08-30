377696ff93dd0785ab5d209f8ada20a8ac33ffdd
Hend Sayed <hendsayed853@gmail.com>
2025-08-30 23:11:43 +0300

    feat: Add CraftoRadius to AppTheme

---
+++ composeApp/src/commonMain/kotlin/org/example/project/designSystem/radius/CraftoRadius.kt
@@ -0,0 +1,19 @@
+package org.example.project.designSystem.radius
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class CraftoRadius(
+    val none : Dp = 0.dp,
+    val x2s : Dp = 2.dp,
+    val xs : Dp =4.dp,
+    val s : Dp = 8.dp,
+    val md : Dp = 10.dp,
+    val lg : Dp = 12.dp,
+    val xl : Dp = 16.dp,
+    val x2l : Dp = 20.dp,
+    val x3l : Dp = 24.dp,
+    val x4l : Dp = 28.dp,
+    val x5l : Dp = 32.dp,
+    val full : Dp = 1000.dp
+)
