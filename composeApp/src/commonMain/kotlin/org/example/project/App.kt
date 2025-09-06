6ceb91ba2020668fecff311d9b91d051b94a0c1e
norhanadel <norhanadel367@gmail.com>
2025-09-07 01:11:04 +0300

    correct app file

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -1,77 +1,49 @@
 package org.example.project
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Icon
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import crafto.composeapp.generated.resources.Res
-import crafto.composeapp.generated.resources.ic_user
-import org.example.project.designSystem.components.TextField
-import org.example.project.designSystem.textStyle.AppTheme
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
+
+import crafto.composeapp.generated.resources.Res
+import crafto.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
-    AppTheme {
+    MaterialTheme {
+        var showContent by remember { mutableStateOf(false) }
         Column(
             modifier = Modifier
-                .padding(20.dp)
+                .background(MaterialTheme.colorScheme.primaryContainer)
+                .safeContentPadding()
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically)
         ) {
-            var text1 by remember { mutableStateOf("") }
-            TextField(
-                labelText = "Label",
-                hint = "Value",
-                text = text1,
-                onTextChange = { text1 = it },
-                enabledState = true
-            )
-
-            var text2 by remember { mutableStateOf("Mus") }
-            TextField(
-                labelText = "Label",
-                hint = "Enter value",
-                text = text2,
-                onTextChange = { text2 = it },
-                errorState = true,
-                errorHint = "Error message",
-                enabledState = true
-            )
-
-            var text3 by remember { mutableStateOf("") }
-            TextField(
-                labelText = "Label",
-                hint = "Value",
-                text = text3,
-                onTextChange = { text3 = it },
-                enabledState = true
-            )
-
-            var text4 by remember { mutableStateOf("") }
-            TextField(
-                labelText = "Label",
-                hint = "Value",
-                text = text4,
-                onTextChange = { text4 = it },
-                showAsPassword = true,
-                forgotAction = { },
-                enabledState = true
-            )
+            Button(onClick = { showContent = !showContent }) {
+                Text(" hello Click me!")
+            }
+            AnimatedVisibility(showContent) {
+                val greeting = remember { Greeting().greet() }
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Image(painterResource(Res.drawable.compose_multiplatform), null)
+                    Text("Compose: $greeting")
+                }
+            }
         }
     }
 }
