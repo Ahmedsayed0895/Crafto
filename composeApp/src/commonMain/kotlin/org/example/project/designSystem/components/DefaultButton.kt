a14d9935ec9a8197f6209f1bf39e38b7eb5a1bc6
nada <nadamansoursamir@gmail.com>
2025-09-08 07:21:44 +0300

    refactor to add previews and corner radius to button components

--- composeApp/src/commonMain/kotlin/org/example/project/designSystem/components/DefaultButton.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/designSystem/components/DefaultButton.kt
@@ -2,9 +2,9 @@ package org.example.project.designSystem.components
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -16,8 +16,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import org.example.project.designSystem.textStyle.AppTheme
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 
 enum class ButtonState {
@@ -29,19 +31,20 @@ enum class ButtonState {
 fun DefaultButton(
     modifier: Modifier = Modifier,
     text: String,
-    icon: Painter?=null,
-    buttonState:ButtonState=ButtonState.DISABLED,
+    icon: Painter? = null,
+    buttonState: ButtonState = ButtonState.DISABLED,
     enabled: Boolean,
     colors: ButtonColors = ButtonDefaults.buttonColors(),
     onClick: () -> Unit,
-    contentPadding: PaddingValues=ButtonDefaults.ContentPadding
+    cornerRadius: Dp = AppTheme.craftoRadius.full,
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding
 ) {
     Button(
         onClick = { onClick() },
         modifier = modifier,
         enabled = enabled,
-        shape = RoundedCornerShape(AppTheme.craftoRadius.full),
-        colors =colors,
+        shape = RoundedCornerShape(cornerRadius),
+        colors = colors,
         contentPadding = contentPadding,
         content = {
             AnimatedVisibility(buttonState == ButtonState.LOADING) {
@@ -52,7 +55,7 @@ fun DefaultButton(
                     verticalAlignment = Alignment.CenterVertically,
                     horizontalArrangement = Arrangement.spacedBy(8.dp)
                 ) {
-                    if(icon!=null){
+                    if (icon != null) {
                         Icon(
                             modifier = Modifier.size(20.dp),
                             painter = icon,
@@ -69,4 +72,53 @@ fun DefaultButton(
         }
     )
 
+}
+
+@Preview
+@Composable
+private fun DefaultButtonPreview(){
+    AppTheme{
+        Column {
+            DefaultButton(
+                text = "Secondary Button",
+                enabled = true,
+                buttonState = ButtonState.Enable,
+                contentPadding = PaddingValues(vertical = 15.dp, horizontal = 24.dp),
+                colors= ButtonDefaults.buttonColors(
+                    containerColor = AppTheme.craftoColors.additional.primaryRed,
+                    contentColor = AppTheme.craftoColors.background.card,
+                    disabledContainerColor = AppTheme.craftoColors.background.card,
+                    disabledContentColor = AppTheme.craftoColors.shade.secondary
+                ) ,
+                onClick = {}
+            )
+            DefaultButton(
+                text = "Secondary Button",
+                enabled = false,
+                buttonState = ButtonState.DISABLED,
+                contentPadding = PaddingValues(vertical = 15.dp, horizontal = 24.dp),
+                colors= ButtonDefaults.buttonColors(
+                    containerColor = AppTheme.craftoColors.additional.primaryBlue,
+                    contentColor = AppTheme.craftoColors.background.card,
+                    disabledContainerColor = AppTheme.craftoColors.background.card,
+                    disabledContentColor = AppTheme.craftoColors.shade.secondary
+                ) ,
+                onClick = {}
+            )
+            DefaultButton(
+                text = "Secondary Button",
+                enabled = true,
+                buttonState = ButtonState.LOADING,
+                contentPadding = PaddingValues(vertical = 15.dp, horizontal = 24.dp),
+                colors= ButtonDefaults.buttonColors(
+                    containerColor = AppTheme.craftoColors.button.secondary,
+                    contentColor = AppTheme.craftoColors.button.onSecondary,
+                    disabledContainerColor = AppTheme.craftoColors.background.card,
+                    disabledContentColor = AppTheme.craftoColors.shade.secondary
+                ) ,
+                onClick = {}
+            )
+        }
+
+    }
 }
