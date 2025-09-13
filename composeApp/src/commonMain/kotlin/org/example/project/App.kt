1d4cac73ab00aed545e207a4fc50c1f06efc86e7
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-09-13 22:50:01 +0300

    feat: add MyApp to AndroidManifest.xml

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -1,21 +1,16 @@
 package org.example.project
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import org.example.project.presentation.designsystem.textstyle.AppTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.KoinContext
 
 @Composable
 @Preview
 fun App() {
     AppTheme {
+        KoinContext {
 
+        }
     }
 }
