59584b06e64a490f8435121c1e8d60ce5409f10d
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-09-19 01:20:54 +0300

    feat: add Category domain entity

---
+++ composeApp/src/commonMain/kotlin/org/example/project/domain/entity/Category.kt
@@ -0,0 +1,11 @@
+package org.example.project.domain.entity
+
+import androidx.compose.ui.graphics.Color
+
+data class Category(
+    val id: Int,
+    val title: String,
+    val isSelected: Boolean,
+    val color: Color,
+    val isCustomer: Boolean,
+)
