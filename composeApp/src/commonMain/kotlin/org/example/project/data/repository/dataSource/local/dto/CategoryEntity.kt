817fd9e5313ff30628670393c4711ac61952d936
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-09-19 01:20:10 +0300

    feat: add CategoryEntity

---
+++ composeApp/src/commonMain/kotlin/org/example/project/data/repository/dataSource/local/dto/CategoryEntity.kt
@@ -0,0 +1,11 @@
+package org.example.project.data.repository.dataSource.local.dto
+
+import androidx.compose.ui.graphics.Color
+
+data class CategoryEntity(
+    val id: Int,
+    val title: String,
+    val isSelected: Boolean,
+    val color: Color,
+    val isCustomer: Boolean,
+)
