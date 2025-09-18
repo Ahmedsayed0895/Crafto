338a16a0a527402e01eeb838f1cfe0c295862878
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-09-19 01:21:15 +0300

    feat: add Category mappers

---
+++ composeApp/src/commonMain/kotlin/org/example/project/data/repository/mapper/Category.kt
@@ -0,0 +1,24 @@
+package org.example.project.data.repository.mapper
+
+import org.example.project.data.repository.dataSource.local.dto.CategoryEntity
+import org.example.project.domain.entity.Category
+
+fun Category.toCategoryEntity(): CategoryEntity {
+    return CategoryEntity(
+        id = id,
+        title = title,
+        isSelected = isSelected,
+        color = color,
+        isCustomer = isCustomer
+    )
+}
+
+fun CategoryEntity.toCategoryDomain(): Category {
+    return Category(
+        id = id,
+        title = title,
+        isSelected = isSelected,
+        color = color,
+        isCustomer = isCustomer
+    )
+}
