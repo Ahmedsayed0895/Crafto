e7a2f39044a0a32b08dee04e2fdf37f9b684132f
Ahmed Sayed <ahmedsayed0895@gmail.com>
2025-09-19 18:54:32 +0300

    feat: add color animation extension

--- composeApp/src/commonMain/kotlin/org/example/project/presentation/ui/screens/setupScreens/component/CategoryActionBox.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/presentation/ui/screens/setupScreens/component/CategoryActionBox.kt
@@ -1,8 +1,5 @@
 package org.example.project.presentation.ui.screens.setupScreens.component
 
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.EaseInCirc
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,14 +7,12 @@ import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import org.example.project.data.memory.dataSource.categoryList
-import org.example.project.domain.entity.Category
 import org.example.project.presentation.designsystem.components.Chip
 import org.example.project.presentation.designsystem.textstyle.AppTheme
+import org.example.project.presentation.ui.extension.toAnimatedColor
 import org.example.project.presentation.viewmodel.accountSetup.AccountSetupCategoryState
 import org.example.project.presentation.viewmodel.accountSetup.AccountSetupState
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -36,50 +31,27 @@ fun CategoryActionBox(
 
             state.categoryState.categories.forEach { category ->
                 val isSelected = category.isSelected
-                val (chipColor, textColor) =
-                    chipColorsChanger(isSelected = isSelected, category = category).first to
-                            chipColorsChanger(isSelected = isSelected, category = category).second
                 Chip(
                     text = category.title,
                     isSelected = isSelected,
                     onChipSelected = { onChipSelected(category.id) },
                     modifier = Modifier.background(
-                        color = chipColor,
+                        color = category.color.toAnimatedColor(
+                            isSelected,
+                            AppTheme.craftoColors.background.card
+                        ),
                         shape = RoundedCornerShape(AppTheme.craftoRadius.full)
                     ),
-                    textColor = textColor
+                    textColor = AppTheme.craftoColors.background.card
+                        .toAnimatedColor(
+                            isSelected,
+                            AppTheme.craftoColors.shade.secondary
+                        )
                 )
             }
         }
     }
 }
-
-@Composable
-private fun chipColorsChanger(isSelected: Boolean, category: Category): Pair<Color, Color> {
-    val chipColor by animateColorAsState(
-        if (isSelected) {
-            category.color
-        } else {
-            AppTheme.craftoColors.background.card
-        }, animationSpec = tween(
-            easing = EaseInCirc
-        )
-    )
-
-
-    val textColor by animateColorAsState(
-        if (isSelected) {
-            AppTheme.craftoColors.background.card
-        } else {
-            AppTheme.craftoColors.shade.secondary
-        }, animationSpec = tween(
-            easing = EaseInCirc,
-
-            )
-    )
-    return chipColor to textColor
-}
-
 
 @Preview
 @Composable
