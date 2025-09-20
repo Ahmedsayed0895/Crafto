b5227b9228e11db2c171777f847d868062440465
Hend Sayed <hendsayed853@gmail.com>
2025-09-20 20:41:41 +0300

    Update logo and phone hint in RegisterScreen

--- composeApp/src/commonMain/kotlin/org/example/project/register/RegisterScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/register/RegisterScreen.kt
@@ -26,8 +26,9 @@ import crafto.composeapp.generated.resources.and_text
 import crafto.composeapp.generated.resources.continue_button
 import crafto.composeapp.generated.resources.egypt_flag
 import crafto.composeapp.generated.resources.enter_phone
-import crafto.composeapp.generated.resources.logo
+import crafto.composeapp.generated.resources.register_logo
 import crafto.composeapp.generated.resources.logo_icon
+import crafto.composeapp.generated.resources.phone_hint
 import crafto.composeapp.generated.resources.privacy_agreement
 import crafto.composeapp.generated.resources.privacy_policy
 import crafto.composeapp.generated.resources.terms_and_conditions
@@ -75,7 +76,7 @@ private fun RegisterContent(
 
             ) {
                 Icon(
-                    painter = painterResource(Res.drawable.logo),
+                    painter = painterResource(Res.drawable.register_logo),
                     contentDescription =stringResource(Res.string.logo_icon),
                     modifier = Modifier.align(Alignment.Center).offset(x = (-5).dp, y = (5).dp),
                     tint = AppTheme.craftoColors.brand.primary
@@ -107,7 +108,7 @@ private fun RegisterContent(
                     )
 
                     TextField(
-                        hint = "+20  000 - 000 - 0000",
+                        hint = stringResource(Res.string.phone_hint),
                         startIcon = {
                             Image(
                                 painter = painterResource(Res.drawable.egypt_flag),
