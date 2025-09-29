703ebd32ae3abf938669f8ee0c21078f14dd1240
norhanadel <norhanadel367@gmail.com>
2025-09-29 18:14:55 +0300

    solve request changes

--- composeApp/src/commonMain/kotlin/org/example/project/register/UserTypeSelectionScreen.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/register/UserTypeSelectionScreen.kt
@@ -6,11 +6,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -22,13 +25,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import crafto.composeapp.generated.resources.Res
 import crafto.composeapp.generated.resources.carftsman
+import crafto.composeapp.generated.resources.craftsman
 import crafto.composeapp.generated.resources.customer
+import crafto.composeapp.generated.resources.how_would_you_like_to_use_san3a
+import crafto.composeapp.generated.resources.i_need_help_with_a_service
+import crafto.composeapp.generated.resources.i_offer_services
+import crafto.composeapp.generated.resources.next
+import crafto.composeapp.generated.resources.you_can_switch_roles_anytime
 import org.example.project.designSystem.components.ButtonState
 import org.example.project.designSystem.components.PrimaryButton
 import org.example.project.designSystem.components.ProgressIndicator
 import org.example.project.designSystem.components.SelectionCard
 import org.example.project.designSystem.textStyle.AppTheme
 import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -40,14 +50,15 @@ fun UserTypeSelectionScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .windowInsetsPadding(WindowInsets.safeDrawing)
             .background(AppTheme.craftoColors.background.screen)
-            .padding(horizontal = 16.dp, vertical = 36.dp),
+            .padding(horizontal = 16.dp, vertical = 16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(32.dp)
     ) {
         ProgressIndicator(
             currentPage = 1,
-            totalPage = 5,
+            totalPage = 4,
             modifier = Modifier
                 .fillMaxWidth(0.75f)
         )
@@ -56,13 +67,13 @@ fun UserTypeSelectionScreen(
             verticalArrangement = Arrangement.Bottom
         ) {
             Text(
-                text = "How would you like to use San3a?",
+                text = stringResource(Res.string.how_would_you_like_to_use_san3a),
                 style = AppTheme.textStyle.display,
                 color = AppTheme.craftoColors.shade.primary
             )
 
             Text(
-                text = "You can switch roles anytime from your profile.",
+                text = stringResource(Res.string.you_can_switch_roles_anytime),
                 style = AppTheme.textStyle.body.largeRegular,
                 color = AppTheme.craftoColors.shade.secondary
             )
@@ -74,8 +85,8 @@ fun UserTypeSelectionScreen(
         ) {
             SelectionCard(
                 img = painterResource(Res.drawable.customer),
-                title = "Customer",
-                caption = "I need help with a service",
+                title = stringResource(Res.string.customer),
+                caption = stringResource(Res.string.i_need_help_with_a_service),
                 isSelected = selectedRole == "Customer",
                 onCardClick = { selectedRole = "Customer" },
                 modifier = Modifier
@@ -86,8 +97,8 @@ fun UserTypeSelectionScreen(
 
             SelectionCard(
                 img = painterResource(Res.drawable.carftsman),
-                title = "Craftsman",
-                caption = "I offer services",
+                title = stringResource(Res.string.craftsman),
+                caption = stringResource(Res.string.i_offer_services),
                 isSelected = selectedRole == "Craftsman",
                 onCardClick = { selectedRole = "Craftsman" },
                 modifier = Modifier
@@ -98,7 +109,7 @@ fun UserTypeSelectionScreen(
         }
 
         PrimaryButton(
-            text = "Next",
+            text = stringResource(Res.string.next),
             enabled = true,
             onClick = onNextClick,
             buttonState = ButtonState.Enable,
@@ -112,9 +123,6 @@ fun UserTypeSelectionScreen(
 @Composable
 private fun UserTypeSelectionScreenPreview() {
     AppTheme(isDarkTheme = false) {
-
         UserTypeSelectionScreen(onNextClick = {})
-
-
     }
 }
