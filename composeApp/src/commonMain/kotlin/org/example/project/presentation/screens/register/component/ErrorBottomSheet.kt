b8ae9487ff8830d8ab367447aa86594619cd7737
Hend Sayed <hendsayed853@gmail.com>
2025-09-29 15:31:50 +0300

    move bottomSheet to to new package

--- composeApp/src/commonMain/kotlin/org/example/project/presentation/screens/register/component/ErrorBottomSheet.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/presentation/screens/register/component/ErrorBottomSheet.kt
@@ -1,4 +1,4 @@
-package org.example.project.presentation.screen.register.component
+package org.example.project.presentation.screens.register.component
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
