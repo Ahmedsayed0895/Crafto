2990d345894ccb7119e99b43c8e6e1ade500ece9
norhanadel <norhanadel367@gmail.com>
2025-10-18 13:10:04 +0300

    edit app file

--- composeApp/src/commonMain/kotlin/org/example/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/App.kt
@@ -3,13 +3,12 @@ package org.example.project
 import androidx.compose.runtime.Composable
 import org.example.project.presentation.designsystem.textstyle.AppTheme
 import org.example.project.presentation.screens.onboarding.OnboardingScreen
-import org.example.project.presentation.screens.setupScreens.location.LocationSetupScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
     AppTheme {
-        LocationSetupScreen()
+        OnboardingScreen()
     }
 }
